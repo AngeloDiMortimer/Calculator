0a3414f3a3b2ff8bbaa017acc1637cbe6e4ed83e
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-12-06 18:02:07 -0400

    changed HTML attributtes in buttons and the output

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const allClearButton = document.querySelector('[data-all-clear]');
+
+const userInputTextElement = document.querySelector('[data-user-input]');
+const resultTextElement = document.querySelector('[data-result]');
