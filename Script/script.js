2625792f494192568a03ad2b8c9cb96b5cad380a
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-12-07 15:25:06 -0400

    changed html attributes user-input and result

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,19 @@
+class Calculator {
+    const constructor = (previousOperandTextElement, currentOperandTextElement) => {
+        
+    }
+
+}
+
+
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-clear]');
 const allClearButton = document.querySelector('[data-all-clear]');
 
-const userInputTextElement = document.querySelector('[data-user-input]');
-const resultTextElement = document.querySelector('[data-result]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
