20d7d366ee82c0f0c60fc943859cef716b7cbf3e
AngeloDiMortimer <angelodimortimer99@gmail.com>
2022-12-07 18:22:30 -0400

    implemented keyboard support and displays numbers

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,39 @@
 class Calculator {
-    const constructor = (previousOperandTextElement, currentOperandTextElement) => {
-        
+    
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
 
+    clear = () => {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+
+    }
+
+    delete = () => {
+
+    }
+
+    appendNumber = (number) => {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation = (operation) => {
+
+    }
+
+    compute = () => {
+
+    }
+
+    updateDisplay = () => {
+        this.currentOperandTextElement.innerText = this.currentOperand; //Updates display everytime a button is pressed
+    }
 }
-
-
-
-
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -17,3 +43,21 @@ const allClearButton = document.querySelector('[data-all-clear]');
 
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+document.documentElement.addEventListener('keydown', (event) => {
+    if ((event.key >= 0 && event.key <= 9) ) {
+        calculator.appendNumber(event.key);
+        calculator.updateDisplay();
+    }
+});
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+
